eb48f006fe2c8ffe4980fd594fba1f7b47dae62e
Shashank yadahalli <74127824+its-shashankY@users.noreply.github.com>
2024-10-06 23:01:47 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,15 +5,6 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCLSzwY_nCUugrbrl9LJbfFQE0ntcDDBUI",
-  authDomain: "pilgrimo-site.firebaseapp.com",
-  projectId: "pilgrimo-site",
-  storageBucket: "pilgrimo-site.appspot.com",
-  messagingSenderId: "524622540521",
-  appId: "1:524622540521:web:bd5a502fe29629945bca78",
-  measurementId: "G-Q8WZN7H3S5"
-};
 
 
 const app = initializeApp(firebaseConfig);
